17a2900c0deab10c7eb5b1db1335f935b3ff41cd
NestorRian <nestor.rian2004@gmail.com>
2025-01-22 22:49:47 -0300

    update

--- flappy.js
+++ flappy.js
@@ -81,11 +81,9 @@ function Passaro(alturaJogo) {
 
     window.onkeydown = e => voando = true
     window.onkeyup = e => voando = false
+    window.ontouchstart = e => voando = true
+    window.ontouchend = e => voando = false
 
-
-    // Para mobile (toque)
-    window.addEventListener('touchstart', () => voando = true);
-    window.addEventListener('touchend', () => voando = false);
 
     this.animar = () => {
         const novoY = this.getY() + (voando ? 8 : -5)
